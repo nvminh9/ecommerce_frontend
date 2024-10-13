c776ca5a89c20cc3170fc9f4a7117862877320f5
minhngo <minhcorn.2@gmail.com>
2024-10-13 21:23:30 +0700

    thêm trang list sản phẩm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
+    {path: "product-list", component: ProductListComponent},
     {path: "product-detail", component: ProductDetailComponent},
-    
     
     {path: "**", component: NotFoundComponent} // route Not Found nên ở cuối cùng
 ];
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
